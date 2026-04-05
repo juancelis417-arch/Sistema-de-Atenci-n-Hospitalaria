ac065e6497f907b963e8a0130b1f9b031bf6ff50
juancelis417-arch <juan.celis417@pascualbravo.edu.co>
2026-04-05 12:09:18 -0500

    sistema

--- src/simulacion.py
+++ src/simulacion.py
@@ -11,12 +11,12 @@ def atender(nombre):
 
         print(f"doctor atendiendo a {nombre}")
         escribir_log(f"Doctor atendiendo a {nombre}")
-        time.sleep(3)
-        print(f'{nombre} fue atendido')
+    time.sleep(3)
+
         
-        with lock:
-         print(f"{nombre} fue atendido")
-         escribir_log(f"{nombre} fue atendido")
+    with lock:
+        print(f"{nombre} fue atendido")
+        escribir_log(f"{nombre} fue atendido")
 
 def simulacion():
     
