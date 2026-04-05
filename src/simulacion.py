a9f632ee6d13418ea2a41c74f6aaa42adc69dba6
juancelis417-arch <juan.celis417@pascualbravo.edu.co>
2026-04-05 12:17:23 -0500

    Sistema-Avances

--- src/simulacion.py
+++ src/simulacion.py
@@ -20,12 +20,42 @@ def atender(nombre):
 
 def simulacion():
     
+    import threading
+import time
+from controlador.datos import pacientes
+from controlador.log import escribir_log
+
+lock = threading.Lock()
+
+def atender(nombre):
     
+    with lock:
+
+        print(f"doctor atendiendo a {nombre}")
+        escribir_log(f"Doctor atendiendo a {nombre}")
+    time.sleep(3)
+
+        
+    with lock:
+        print(f"{nombre} fue atendido")
+        escribir_log(f"{nombre} fue atendido")
+
+def simulacion():
+    
+    
+
+    hilos = []
 
     for p in pacientes:
         hilo = threading.Thread(target=atender, args=(p[0],))
+        hilos.append(hilo)
         hilo.start()
+
+    for h in hilos:
+        h.join()
         
 
+            
+            
             
             
